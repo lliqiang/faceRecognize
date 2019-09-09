f36f941d9637deba558ac3f03dda094c53022101
liqiang <13821062095@163.com>
2019-09-09 10:46:25 +0800

    改FaceRecognition文件

--- facedetect/src/main/java/com/troila/ai/FaceRecognition.java
+++ facedetect/src/main/java/com/troila/ai/FaceRecognition.java
@@ -6,21 +6,20 @@ public class FaceRecognition {
         System.loadLibrary("SeetaFaceLandmarker2");
         System.loadLibrary("SeetaFaceRecognizer2");
         System.loadLibrary("seetanet2");
-
         System.loadLibrary("fr_android");
     }
-	//50 10 0.1
-	
-    public native int initModel(char[] sModelPath,int minFace, int distCacheSize, double distThreshold);
+
+    public native boolean extractModel(char[] srcPath, char[] dstPath);
+
+    public native boolean initModel(char[] sModelPath, int minFace, int distCacheSize, double distThreshold);
+
     //新接口，输出一个15维向量。1-4元素为人脸框位置（左上坐标和右下坐标），5-14为5个关键点坐标，15为姿态判断结果（1是符合，0是不符合）
     public native int[] detectFace(byte[] _pstImageData, int iWidth, int iHeight);
-	//下表 4-13
-	//0 姿态不好 1 姿态好
 
     //最后的int数组传入上面的关键点坐标
-    public native float[] getFaceFeature(byte[] _pstImageData, int iWidth, int iHeight, int[] _pstFAResult);
+    public native float[] getFeature(byte[] _pstImageData, int iWidth, int iHeight, int[] _pstFAResult);
 
-    public native float faceMatch(float[] feats1, float[] feats2);
+    public native float getSimilarity(float[] feats1, float[] feats2);
 
     public native int release();
 
